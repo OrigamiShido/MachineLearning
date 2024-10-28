3d2d1dc210215122efe2647530205be38391e81c
OrigamiShido <113040308+OrigamiShido@users.noreply.github.com>
2024-10-29 00:23:26 +0800

    exp3.1complete

---
+++ exp3/bayesmail.py
@@ -0,0 +1,96 @@
+import random
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import re
+
+from param.parameterized import classlist
+from sklearn.naive_bayes import MultinomialNB
+
+
+def createVocabList(dataSet):
+    vocabSet=set([])
+    for document in dataSet:
+        vocabSet=vocabSet|set(document)
+    return list(vocabSet)
+
+def setOfWrods2Vec(vocabList,inputSet):
+    returnVec=[0]*len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)]=1
+        else:
+            print('the word: %s is not in my Vocabulary!'% word)
+    return returnVec
+
+def trainNB0(trainMatrix, trainCategory):
+    numTrainDocs = len(trainMatrix)
+    numWords = len(trainMatrix[0])
+    pAbusive = sum(trainCategory) / float(numTrainDocs)
+    p0Num = np.zeros(numWords)
+    p1Num = np.zeros(numWords)
+    p0Denom = 0.0
+    p1Denom = 0.0
+    for i in range(numTrainDocs):
+        if trainCategory[i]==1:
+            p1Num+=trainMatrix[i]
+            p1Denom+=sum(trainMatrix[i])
+        else:
+            p0Num+=trainMatrix[i]
+            p1Denom+=sum(trainMatrix[i])
+    p1Vect=p1Num/p1Denom
+    p0Vect=p0Num/p0Denom
+    return p0Vect,p1Vect,pAbusive
+
+def classifyNB(vec2Classify,p0Vec,p1Vec,pClass1):
+    p1=sum(vec2Classify*p1Vec)+np.log(pClass1)
+    p0=sum(vec2Classify*p0Vec)+np.log(1-pClass1)
+    if p1>p0:
+        return 1
+    else:
+        return 0
+
+def bagOfWords2VecMN(vocabList,inputSet):
+    returnVec=[0]*len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)]+=1
+    return returnVec
+
+def textParse(bigString):
+    listOfTokens=re.split(r'\W*',bigString)
+    return [tok.lower() for tok in listOfTokens if len(tok)>2]
+
+def spamTest():
+    docList=[]
+    classList=[]
+    fullText=[]
+    for i in range(1,26):
+        wordList=textParse(open('C:/Users/Admin/Desktop/WHU study/programming/python/MachineLearning/exp3/email/spam/%d.txt' % i).read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(1)
+        wordList=textParse(open('C:/Users/Admin/Desktop/WHU study/programming/python/MachineLearning/exp3/email/ham/%d.txt' % i).read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(0)
+    vocabList=createVocabList(docList)
+    trainingSet=range(50)
+    testSet=[]
+    for i in range(10):
+        randIndex=int(random.uniform(0,len(trainingSet)))
+        testSet.append(list(trainingSet)[randIndex])
+        del(list(trainingSet)[randIndex])
+    trainMat=[]
+    trainClasses=[]
+    for docIndex in trainingSet:
+        trainMat.append(setOfWrods2Vec(vocabList,docList[docIndex]))
+        trainClasses.append(classList[docIndex])
+    p0V,p1V,pSpam=trainNB0(np.array(trainMat),np.array(trainClasses))
+    errorCount=0
+    for docIndex in testSet:
+        wordVector=setOfWrods2Vec(vocabList,docList[docIndex])
+        if classifyNB(np.array(wordVector),p0V,p1V,pSpam)!=classList[docIndex]:
+            errorCount+=1
+    print('the error rate is: ',float(errorCount)/len(testSet))
